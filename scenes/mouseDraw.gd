2ccdacc0504790b95407f817bf58ab3e6722bf67
XtophB <xtophb@gmail.com>
2026-05-30 01:14:20 +0200

    feat: add tracking of points reached by cursor

--- scenes/mouseDraw.gd
+++ scenes/mouseDraw.gd
@@ -2,10 +2,20 @@ extends Node2D
 
 
 var mousePositions: Array = []
+var markerContainer: Node2D
+var markerList: Array = []
+var markerListBoolean: Array[bool]
+const DISTANCE_THRESHOLD: int = 10
 
-
+var markerBoolean: Array[bool] = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	markerList = get_node("markerContainer").get_children()
+	markerListBoolean.resize(markerList.size())
+	markerListBoolean.fill(false)
+
+	for x in markerList:
+		print(x.position)
 	pass # Replace with function body.
 
 
@@ -19,8 +29,25 @@ func _input(event: InputEvent) -> void:
 	
 	mousePositions.append(event.position)
 	queue_redraw()
+	_checkDistance(event.position)
+	
 
 func _draw() -> void:
 	for coord in mousePositions:
 		draw_circle(coord, 10, Color.RED)
-		
+
+
+func _checkDistance(position) -> void:
+	for each in markerList:
+		if position.distance_to(each.global_position) < DISTANCE_THRESHOLD:
+			print("Reached node " , each , " at: " , each.global_position)
+			var index = markerList.find(each)
+			markerListBoolean[index] = true
+			printBooleans()
+			
+
+func printBooleans() -> void:
+	var i = 0
+	for x in markerListBoolean:
+		print("position ", i, markerListBoolean[i])
+		i = i+1
