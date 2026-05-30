9d9af70740e61aa8f1470cfbe8cde93fb9cbcd38
XtophB <xtophb@gmail.com>
2026-05-31 00:03:15 +0200

    feat: add points display

--- scripts/main_test.gd
+++ scripts/main_test.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 var playerPoints = 0
+@onready var pointsHUD = $HUD/PointsAmount
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -18,10 +19,13 @@ func _on_sealskater_graffiti_fuel_updated(amount: float) -> void:
 func _on_sealskater_spray_can_amount_consumed_for_points(points: float) -> void:
 	playerPoints += points
 	print("Player Points: ", playerPoints)
+	pointsHUD.text = str(int(playerPoints))
 
 func _on_node_2d_pass_points(points: float) -> void:
 	playerPoints += points
 	print("Player Points: ", playerPoints)
+	pointsHUD.text = str(int(playerPoints))
 
 
-func instantiate 
+func instantiateMinigame() -> void:
+	pass
