33d7c55cbfb21e0a7de10dd7503a8b937dbb1809
Philipp Wolfger <philipp.wolfger@alturos.com>
2026-05-30 13:45:17 +0200

    added interface for graffiti fuel update to support multi developer workflow

--- scripts/sealskater.gd
+++ scripts/sealskater.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 @onready var movementController := %MovementController
 @onready var grindingController := %GrindingController
 
+signal graffiti_fuel_updated(amount: float)
 
 func _physics_process(_delta: float) -> void:
 	grindingController.handle_grinding()
