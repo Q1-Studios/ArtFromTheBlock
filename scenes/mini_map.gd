96361808453d13d7002267780c95829bf0a9fc37
David <ds@mscg.at>
2026-05-31 09:44:49 +0200

    Mini Map update

---
+++ scenes/mini_map.gd
@@ -0,0 +1,20 @@
+extends MarginContainer
+
+
+@export var target: Node3D 
+@export var camera: Camera3D
+
+
+var offset:Vector3
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if(target==null): 
+		print("Noo Target !!")
+		return
+	offset = camera.global_position - target.position
+		
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	camera.global_position = target.global_position + offset 
